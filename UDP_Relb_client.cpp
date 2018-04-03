4bb59f59168ca0a067e7c730d5c13fa02e2cb76a
Unknown <nabiyuni@iu.edu>
2018-04-02 22:10:41 -0400

    final version

--- UDP_Relb_client.cpp
+++ UDP_Relb_client.cpp
@@ -2,11 +2,12 @@
 #include <fstream>
 #include <stdlib.h>
 #include <string.h>
+#include <sstream>
 #include <unistd.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-#include <regex>
+//#include <regex>
 #include <netdb.h>
 <<<<<<< master
 //#include <Windows.h>
@@ -20,7 +21,7 @@
 #include <chrono>
 #include <thread>
 //#include <ctime>
-#include <time.h>
+#include <sys/time.h>
 #include <cstdlib>
 >>>>>>> Removing regex
 
@@ -37,23 +38,45 @@ struct header_struct{
     header_struct(int32_t a, int32_t b, bool c, bool d) : squnc_number(a), ack_number(b), ack_flag(c), fin_flag(d) {}
 };
 void add_header(header_struct &sendg_header, stringstream& stream){
-    char header[22];
+    char header[23];
     sprintf (header, "%10d%10d%1d%1d", sendg_header.squnc_number, sendg_header.ack_number,
              sendg_header.ack_flag, sendg_header.fin_flag);
     stream << header;
 }
-void extract_Header(char* buffer, header_struct& recvg_header, int& buffer_size){
+void extract_Header(char* buffer, header_struct& recvg_header, int& buffer_size, bool& acptHeader){
     int numbers_size = 10;
     int flag_size = 1;
     int headerSize = 2 * numbers_size + 2 * flag_size;
     char temp[numbers_size];
     memmove (temp, buffer , numbers_size);
-    recvg_header.squnc_number = atoi(temp);
+    try {
+        recvg_header.squnc_number = stoi(temp);
+    }
+    catch(...) {
+        acptHeader = false;
+    }
     memmove (temp, buffer + numbers_size , numbers_size);
-    recvg_header.ack_number = atoi(temp);
+    try {
+        recvg_header.ack_number = stoi(temp);
+    }
+    catch(...) {
+        acptHeader = false;
+    }
     memmove (temp, buffer + 2* numbers_size , flag_size);
+    try {
+        int test = stoi(temp);
+    }
+    catch(...) {
+        acptHeader = false;
+    }
     recvg_header.ack_flag = (temp[0] == '1') ? true:false;
     memmove (temp, buffer + 2* numbers_size + flag_size , flag_size);
+    try {
+        int test = stoi(temp);
+    }
+    catch(...) {
+        acptHeader = false;
+    }
     recvg_header.fin_flag = (temp[0] == '1') ? true:false;
     memmove (buffer, buffer + headerSize , buffer_size);// = buffer(m.position(),str.length());
 }
@@ -69,6 +92,7 @@ void add_Date(stringstream *stream){
     *stream << crrnt_Time;
 }
 
+<<<<<<< master
 int main(int argc, char *argv[]) {
 
     int sockfd, portno, n;
@@ -104,6 +128,13 @@ int main(int argc, char *argv[]) {
 /*
     int clientSocket, rcvd_Data, server_port;
     int bufsize = atoi(argv[6]);
+=======
+int main(int arg_Num, char *args[]) {//){//
+    fflush(stdout);
+    int clientSocket, rcvd_Data, server_port;
+    int bufsize = atoi(args[6]);
+    //int bufsize = 150000;
+>>>>>>> final version
     char buffer[bufsize];
     const char *server_host, *filename;// = malloc(bufsize);
     struct sockaddr_in srvr_Addr, clnt_Addr;
@@ -124,7 +155,7 @@ int main(int argc, char *argv[]) {
 /*
     server_host = "192.168.2.223";//149.160.210.223";//192.168.2.223";//149.160.200.10";//
     server_port = 15010;//
-    connection_type = "non-persistent";//
+    //connection_type = "non-persistent";//
     filename = "text_1MB.txt";//text_1MB.txt";//HelloWorld.html";//
 */
     /*
@@ -142,8 +173,18 @@ int main(int argc, char *argv[]) {
     string compPattern = zeroTo255 + "\\." + zeroTo255 + "\\." + zeroTo255 + "\\." + zeroTo255;
     regex pattern(compPattern);//"(\\d{1,3})\.(\\d{1,3})");
 */
+<<<<<<< master
 >>>>>>> Removing regex
     clock_t startTime = clock(); //Start timer
+=======
+    struct timespec start;//,temp;
+    clock_gettime(CLOCK_REALTIME, &start);
+    long nsecs = start.tv_nsec;
+    long secs = start.tv_sec;
+    clock_t startTime = clock();
+    //struct timespec st, end;
+    //clock_gettime(CLOCK_MONOTONIC_RAW, &st);
+>>>>>>> final version
     if ((clientSocket = socket(AF_INET, SOCK_DGRAM, 0)) > 0){
         printf("The socket was created\n");
     }
@@ -197,12 +238,13 @@ int main(int argc, char *argv[]) {
 
 
     struct header_struct recvg_header;
-    extract_Header(buffer, recvg_header, bufsize);
+    bool is_header = true;
+    extract_Header(buffer, recvg_header, bufsize, is_header);
 
-    regex okay_Pttrn("200 OK");
-    regex notFnd_Pttrn("404 Not Found");
+    //regex okay_Pttrn("200 OK");
+    //regex notFnd_Pttrn("404 Not Found");
     //regex pat_connct_len("Content-length:.+\n");
-    smatch m;
+    //smatch m;
     if (rcvd_Data < 0) perror("ERROR reading from socket");
     printf("Here is the server message:\n %s\n", buffer);
     string str(buffer);
@@ -237,18 +279,26 @@ int main(int argc, char *argv[]) {
          do{
              rand_drop = rand() % 100 + 1;
              rcvd_Data = recvfrom(clientSocket, buffer , bufsize, 0, (struct sockaddr *)&srvr_Addr, &srvr_Len);
+<<<<<<< master
              //std::this_thread::sleep_for(std::chrono::microseconds(atoi(argv[5])));
              //Sleep(stod(args[5]));
 	     usleep(stod(argv[5]));
              if (rcvd_Data > 0  && rand_drop >= atoi(argv[4])) {
+=======
+             std::this_thread::sleep_for(std::chrono::microseconds(atoi(args[5])));//500));//
+             //Sleep(stod(args[5]));
+             if (rcvd_Data > 0  && rand_drop >= atoi(args[4])) {//60){//
+>>>>>>> final version
                  stringstream stream;
                  //printf("\nreceived header: %.*s\n", header_size, buffer);
-                 extract_Header(buffer, recvg_header, bufsize);
-                 if (LastByteAcked == recvg_header.squnc_number){
+                 bool is_header = true;
+                 extract_Header(buffer, recvg_header, bufsize, is_header);
+                 if (is_header && LastByteAcked == recvg_header.squnc_number){
                      LastByteAcked = recvg_header.squnc_number + rcvd_Data - header_size;
                  }
+                 else if (!is_header) printf("bad header from the server!\n");
                  else{
-                    //printf("LastByeAcked did not get updated\n");
+                    //printf("LastByeAcked did not get updated LastByteAcked: %d recvg_header.squnc_number: %d\n",LastByteAcked, recvg_header.squnc_number);
                  }
                  //printf("Acke: %d, header sequ: %d, header ack: %d\n",
                  //       LastByteAcked, recvg_header.squnc_number, recvg_header.ack_number);
@@ -256,6 +306,12 @@ int main(int argc, char *argv[]) {
                  add_header(sendg_header, stream);
                  sending_Text = stream.str();
                  sendto(clientSocket, sending_Text.c_str(), sending_Text.length(),0, (struct sockaddr *)&srvr_Addr, srvr_Len);
+                 //printf("sent ack: %s\n", stream.str().c_str());
+                 if (recvg_header.fin_flag == true) {
+                     sendto(clientSocket, sending_Text.c_str(), sending_Text.length(), 0,
+                            (struct sockaddr *) &srvr_Addr, srvr_Len);
+                     //printf("sent ack: %s\n", stream.str().c_str());
+                 }
 
              }
              else {
@@ -275,7 +331,10 @@ int main(int argc, char *argv[]) {
         //printf("number of received bytes: %d\n", LastByteAcked);
         //printf("The file was received and elapsed time(msec)is: %10.3f\n", double(clock() - startTime) * 1000 / CLOCKS_PER_SEC);
     	std::cout << "number of received bytes: " << LastByteAcked << std::endl;
-    	std::cout << "The file was received and elapsed time(msec)is: " << (clock() - startTime) / 1000<< std::endl;
+        struct timespec end;//,temp;
+        clock_gettime(CLOCK_REALTIME, &end);
+        uint64_t delta_us = (end.tv_sec - secs) * 1000000 + (end.tv_nsec - nsecs) / 1000;
+    	std::cout << "The file was received and elapsed time(msec)is: " << delta_us / 1000<<endl;//int((stop.tv_usec - start.tv_usec) /1000) <<endl;//(clock() - startTime) / 1000 << std::endl;//
     }
     else if (str.substr(0, 13) == "404 Not Found") {
         printf("Server could not find the requested file!\n");
--- test.cpp
+++ test.cpp
@@ -4,21 +4,17 @@
 #include <stdlib.h>
 #include <string.h>
 #include <string>
-
+#include <unistd.h>
+#include <time.h>
+#include <stdint.h>
 using namespace std;
 
 //
 // Created by nabiyuni on 3/23/2018.
 //
 main() {
-    char buffer[] = "Filename:somt.txt\n";
-    //std::regex pattern("/.*");
-    //smatch m;
-    printf("Here is the client message:\n%s\n", buffer);
-    string file_Path(buffer);
-    //char part[8];
-    char part[10];
-    memcpy(part, buffer, 8);
-    if (file_Path.substr(0, 9) == "Filename:") {printf(file_Path.substr(9, file_Path.size()-10).c_str());}
-    else printf(file_Path.c_str());
+    uint64_t a=120;
+    uint64_t b = 200;
+    uint64_t temp= int(0.875 * a + 0.125 * b);
+    printf("%d",temp);
 }
